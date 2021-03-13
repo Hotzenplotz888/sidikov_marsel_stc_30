778dd43432aaded1e1c67583bc51c235d116c9e7
Eugene Sofronov <hotzenplotz888@gmail.com>
2021-03-13 18:42:02 +0900

    2.HW2 done

--- HomeWork04/HW2.java
+++ HomeWork04/HW2.java
@@ -1,7 +1,29 @@
 //Задание 2.
 //Реализовать метод бинарного поиска с помощью рекурсии.
-class Program {
-	public static int binarySearch(int array[], int element) {
-	...
+class HW2 {
+
+	int binSearchRec(int myArr[], int left, int right, int key){
+		if (right >= left){
+			int mid = left + (right - left) / 2;
+			if (myArr[mid] == key)
+				return mid;
+			if (myArr[mid] > key)
+				return binSearchRec(myArr, left, mid - 1, key);
+			return binSearchRec(myArr, mid + 1, right, key);
+		}
+		return -1;
 	}
+	public static void main(String args[]){
+		HW2 myObject = new HW2();
+		int myArr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+		//int myArr[] = { 6, 2, 3, 7, 5, 6, 4, 8, 9, 1};
+		int length = myArr.length;
+		int key = 4;
+		int result = myObject.binSearchRec(myArr, 0, length - 1, key);
+		if (result == -1)
+			System.out.println("No key");
+		else
+			System.out.println("Key is at index: " + result);
+	}
+
 }
